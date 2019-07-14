e8d46e7ddefcd4cc2b851dfed5d73c0053d7a7b9
Timothée Jaussoin <edhelas@movim.eu>
2019-07-14 13:53:15 +0200

    Inject the table after the docmeta element in the XEP pages Syntax and indentation

--- xmpp.org/doap.js
+++ xmpp.org/doap.js
@@ -1,114 +1,174 @@
-const rdf_ns = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#';
-const doap_ns = 'http://usefulinc.com/ns/doap#';
-const foaf_ns = 'http://xmlns.com/foaf/0.1/';
-const xmpp_ns = 'https://linkmauve.fr/ns/xmpp-doap#';
-const xhtml_ns = 'http://www.w3.org/1999/xhtml';
+const rdfNS = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#';
+const doapNS = 'http://usefulinc.com/ns/doap#';
+const foafNS = 'http://xmlns.com/foaf/0.1/';
+const xmppNS = 'https://linkmauve.fr/ns/xmpp-doap#';
+const xhtmlNS = 'http://www.w3.org/1999/xhtml';
 
-function get_element(elem, ns, name) {
-	const list = elem.getElementsByTagNameNS(ns, name)
-	if (list.length < 1)
-		return null;
-	return list[0];
+function getElement(elem, ns, name) {
+    const list = elem.getElementsByTagNameNS(ns, name)
+    if (list.length < 1) return null;
+
+    return list[0];
 }
 
-function get_element_resource(elem, ns, name) {
-	const list = elem.getElementsByTagNameNS(ns, name)
-	if (list.length < 1)
-		return null;
-	const elem2 = list[0];
-	if (elem2 === undefined)
-		return null;
-	return elem2.getAttributeNS(rdf_ns, 'resource');
+function getElementResource(elem, ns, name) {
+    const list = elem.getElementsByTagNameNS(ns, name)
+    if (list.length < 1) return null;
+
+    const elem2 = list[0];
+    if (elem2 === undefined) return null;
+
+    return elem2.getAttributeNS(rdfNS, 'resource');
 }
 
-function get_element_text(elem, ns, name) {
-	const list = elem.getElementsByTagNameNS(ns, name)
-	if (list.length < 1)
-		return null;
-	const elem2 = list[0];
-	if (elem2 === undefined)
-		return null;
-	return elem2.textContent;
+function getElementText(elem, ns, name) {
+    const list = elem.getElementsByTagNameNS(ns, name)
+    if (list.length < 1) return null;
+
+    const elem2 = list[0];
+    if (elem2 === undefined) return null;
+
+    return elem2.textContent;
 }
 
-function parse_doap() {
-	const xml = this.responseXML;
-	const elem = xml.documentElement;
-	const project = get_element(elem, doap_ns, 'Project');
-	const name = get_element_text(elem, doap_ns, 'name');
-	const homepage = get_element_resource(elem, doap_ns, 'homepage');
-	const xmpp = get_element(elem, xmpp_ns, 'software');
-	let type_elem = get_element(xmpp, xmpp_ns, 'Client');
-	if (type_elem === undefined)
-		type_elem = get_element(elem, xmpp_ns, 'Server');
-	if (type_elem === undefined)
-		type_elem = get_element(elem, xmpp_ns, 'Library');
-	if (type_elem === undefined)
-		type_elem = get_element(elem, xmpp_ns, 'Component');
-	const type = type_elem.localName;
-	const supports = type_elem.getElementsByTagNameNS(xmpp_ns, 'supports');
-	const software = {
-		name: name,
-		homepage: homepage,
-		type: type,
-		xeps: {},
-	};
-	for (let support of supports) {
-		const xep = get_element(support, xmpp_ns, 'SupportedXep');
-		const xep_url = get_element_resource(xep, xmpp_ns, 'xep');
-		const [_, number] = /https:\/\/xmpp\.org\/extensions\/xep-(\d\d\d\d)\.html/.exec(xep_url);
-		const status = get_element_text(xep, xmpp_ns, 'status');
-		const version = get_element_text(xep, xmpp_ns, 'version');
-		const since = get_element_text(xep, xmpp_ns, 'since');
-		const note = get_element_text(xep, xmpp_ns, 'note');
-		software.xeps[number] = {
-			number: number,
-			status: status,
-			version: version,
-			since: since,
-			note: note,
-		};
-	}
+function parseDoap() {
+    const xml = this.responseXML;
+    const elem = xml.documentElement;
+    const project = getElement(elem, doapNS, 'Project');
+    const name = getElementText(elem, doapNS, 'name');
+    const homepage = getElementResource(elem, doapNS, 'homepage');
+    const xmpp = getElement(elem, xmppNS, 'software');
+    let typeElem = getElement(xmpp, xmppNS, 'Client');
+    if (typeElem === undefined)
+        typeElem = getElement(elem, xmppNS, 'Server');
+    if (typeElem === undefined)
+        typeElem = getElement(elem, xmppNS, 'Library');
+    if (typeElem === undefined)
+        typeElem = getElement(elem, xmppNS, 'Component');
+    const type = typeElem.localName;
+    const supports = typeElem.getElementsByTagNameNS(xmppNS, 'supports');
+    const software = {
+        name: name,
+        homepage: homepage,
+        type: type,
+        xeps: {},
+    };
 
-	update_display(software);
+    for (let support of supports) {
+        const xep = getElement(support, xmppNS, 'SupportedXep');
+        const xep_url = getElementResource(xep, xmppNS, 'xep');
+        const [_, number] = /https:\/\/xmpp\.org\/extensions\/xep-(\d\d\d\d)\.html/.exec(xep_url);
+        const status = getElementText(xep, xmppNS, 'status');
+        const version = getElementText(xep, xmppNS, 'version');
+        const since = getElementText(xep, xmppNS, 'since');
+        const note = getElementText(xep, xmppNS, 'note');
+        software.xeps[number] = {
+            number: number,
+            status: status,
+            version: version,
+            since: since,
+            note: note,
+        };
+    }
+
+    updateDisplay(software);
 }
 
-function send_request(filename) {
-	const xhr = new window.XMLHttpRequest();
-	xhr.addEventListener('load', parse_doap);
-	xhr.open('GET', filename);
-	xhr.send();
+function sendRequest(filename) {
+    const xhr = new window.XMLHttpRequest();
+    xhr.addEventListener('load', parseDoap);
+    xhr.open('GET', filename);
+    xhr.send();
 }
 
-function update_display(software) {
-	const {name, homepage} = software;
-	const [_, number] = /xep-(\d\d\d\d)\.(?:x|ht)ml/.exec(document.location);
-	const xep = software.xeps[number];
-	const {status, version, since, note} = xep;
-	console.log(name, homepage, status, version, since, note);
-	const revision = document.getElementById('revision-history-v' + version);
-	let support = document.getElementById('revision-history-v' + version + '-support');
-	if (support === null) {
-		const aside = document.createElementNS(xhtml_ns, 'aside');
-		aside.style.border = '1px solid black';
-		aside.style.float = 'right';
-		const p = document.createElementNS(xhtml_ns, 'p');
-		p.textContent = 'Support';
-		aside.append(p);
-		support = document.createElementNS(xhtml_ns, 'ul');
-		support.setAttributeNS(null, 'id', 'revision-history-v' + version + '-support');
-		aside.appendChild(support);
-		revision.prepend(aside);
-	}
-	const li = document.createElementNS(xhtml_ns, 'li');
-	const software_elem = document.createElementNS(xhtml_ns, 'a');
-	software_elem.setAttributeNS(null, 'href', homepage);
-	software_elem.textContent = name;
-	li.appendChild(software_elem);
-	support.appendChild(li);
+function updateDisplay(software) {
+    const {name, homepage, type} = software;
+    const [_, number] = /xep-(\d\d\d\d)\.(?:x|ht)ml/.exec(document.location);
+    const xep = software.xeps[number];
+    const {status, version, since, note} = xep;
+
+    const docmeta = document.querySelector('.docmeta-wrap');
+    let support = document.querySelector('figure#support');
+
+    if (support === null) {
+        const figure = document.createElementNS(xhtmlNS, 'figure');
+        figure.id = 'support';
+        figure.classList.add('table');
+
+        const figureCaption = document.createElementNS(xhtmlNS, 'figcaption');
+        figureCaption.id = 'support';
+        figureCaption.textContent = 'Software support'
+        figure.appendChild(figureCaption);
+
+        const table = document.createElementNS(xhtmlNS, 'table');
+        figure.appendChild(table);
+
+        const tbody = document.createElementNS(xhtmlNS, 'tbody');
+        table.appendChild(tbody);
+
+        const tr = document.createElementNS(xhtmlNS, 'tr');
+        tr.classList.add('body');
+        tbody.appendChild(tr);
+
+        const thSoftware = document.createElementNS(xhtmlNS, 'th');
+        thSoftware.textContent = 'Software';
+        tr.appendChild(thSoftware);
+
+        const thType = document.createElementNS(xhtmlNS, 'th');
+        thType.textContent = 'Type';
+        tr.appendChild(thType);
+
+        const thVersion = document.createElementNS(xhtmlNS, 'th');
+        thVersion.textContent = 'Version';
+        tr.appendChild(thVersion);
+
+        const thSupport = document.createElementNS(xhtmlNS, 'th');
+        thSupport.textContent = 'Support';
+        tr.appendChild(thSupport);
+
+        docmeta.after(figure);
+
+        support = figure;
+    }
+
+    const tbody = support.querySelector('tbody');
+
+    const tr = document.createElementNS(xhtmlNS, 'tr');
+    tr.classList.add('body');
+    tbody.appendChild(tr);
+
+    const tdClient = document.createElementNS(xhtmlNS, 'td');
+    const aClient = document.createElementNS(xhtmlNS, 'a');
+    aClient.href = homepage;
+    aClient.textContent = name;
+    tdClient.appendChild(aClient);
+    tr.appendChild(tdClient);
+
+    const tdType = document.createElementNS(xhtmlNS, 'td');
+    tdType.textContent = type;
+    tr.appendChild(tdType);
+
+    const tdVersion = document.createElementNS(xhtmlNS, 'td');
+    const aVersion = document.createElementNS(xhtmlNS, 'a');
+    aVersion.href = '#revision-history-v' + version;
+    aVersion.textContent = version;
+    tdVersion.appendChild(aVersion);
+    tr.appendChild(tdVersion);
+
+    const tdSupport = document.createElementNS(xhtmlNS, 'td');
+    tdSupport.textContent = status;
+
+    if (note !== null) {
+        const abbr = document.createElementNS(xhtmlNS, 'abbr');
+        abbr.textContent = '*';
+        abbr.title = note;
+        tdSupport.appendChild(abbr);
+    }
+
+    tr.appendChild(tdSupport);
 }
 
 (function(){
-	send_request('../poezio.xml');
-	send_request('../movim.xml');
+    sendRequest('../poezio.xml');
+    sendRequest('../movim.xml');
 })();
